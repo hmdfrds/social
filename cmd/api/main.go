e7f9ee642b5426f4099ae5e6786f3c253ac09e32
Firdaus <firdaus1337@gmail.com>
2025-03-13 20:52:27 +0800

    repository pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/hmdfrds/social/internal/env"
+	"github.com/hmdfrds/social/internal/store"
 	"github.com/joho/godotenv"
 )
 
@@ -14,10 +15,11 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8081"),
 	}
+	store := store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
-
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
